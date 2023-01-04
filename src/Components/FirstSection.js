875ff2297cd41aa83edd8255506538e993f5242a
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-04 15:56:27 -0300

    adicionando imagens teste do carrossel

--- src/Components/FirstSection.js
+++ src/Components/FirstSection.js
@@ -1,6 +1,7 @@
-//import { Carousel } from 'bootstrap';
+import './FirstSection.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-bootstrap/Carousel'
+
 
 function FirstSection() {
     return (
@@ -8,18 +9,19 @@ function FirstSection() {
         <Carousel.Item interval={1000}>
           <img
             className="d-block w-100"
-            src="https://www.youtube.com/watch?v=Q-dabTRz53s&list=RDQ-dabTRz53s&start_radio=1&ab_channel=TarcisiodoAcordeon"
+            src="https://images.unsplash.com/photo-1642650350264-19d893dbb258?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
             alt="First slide"
+            
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3 className='titulo'>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
+            src="https://images.unsplash.com/photo-1507146153580-69a1fe6d8aa1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt="Second slide"
           />
           <Carousel.Caption>
@@ -30,7 +32,7 @@ function FirstSection() {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
             alt="Third slide"
           />
           <Carousel.Caption>
