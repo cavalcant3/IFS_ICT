8f27dc737a57d2c4f5ba33fda222fe5a32bf86dc
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-05 09:25:12 -0300

    alterações no navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,11 +7,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from './imagens/teste2.png'
+//import './Header.css'
 function Header(){
     return(
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-      <NavbarBrand href="#home">
+   
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+      <Container style={{paddingLeft: '50px', paddingRight: '50px'}}>
+    
+        <NavbarBrand href="#home">
       <img
         alt="logo"
         src= {logo}
@@ -28,18 +31,21 @@ function Header(){
            
             
           </Nav>
-          <Nav>
-          <Nav.Link href="#Inicio">Inicio</Nav.Link>
-          <NavDropdown title="Cursos" id="collasible-nav-dropdown">
+          <Nav className='navbarrr' >
+          <Nav.Link href="#Inicio" style={{color : '#ffff'}}>Inicio</Nav.Link>
+          <NavDropdown title="Cursos" style={{color : '#ffff'}} id="collasible-nav-dropdown" >
               <NavDropdown.Item href="#action/3.1">Inteligência artificial</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#Contato">Contato</Nav.Link>
-            <Nav.Link href="#deets">Sala virtual</Nav.Link>
+            <Nav.Link href="#Contato" style={{color : '#ffff'}}>Contato</Nav.Link>
+            <Nav.Link href="#deets" style={{color : '#ffff'}}>Sala virtual</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
+      
       </Container>
     </Navbar>
-        //<><h1>OI MUNDO</h1><Button>Matheus</Button></>
+  
+       
     );
 }
 export default Header;
