731514b5a5ce934bad530c39044ae7715216e703
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-07 08:29:22 -0300

    Component de cardProfile para perfis

---
+++ src/Components/CardProfile.js
@@ -0,0 +1,13 @@
+import './CardProfile.css'
+
+function CardProfile(props) {
+    return(
+        <div className="CardProfile">
+      <img src={props.profileImageUrl} alt="Profile" />
+      <h1>{props.name}</h1>
+      <p>{props.bio}</p>
+    </div>
+    );
+
+}
+export default CardProfile;
