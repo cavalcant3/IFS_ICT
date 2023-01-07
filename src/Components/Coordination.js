6c78754d1f9edd74a873d42d6d1546f21295552d
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-07 08:30:22 -0300

    Definindo nova seção da coordenação e usando o componente cardProfile

---
+++ src/Components/Coordination.js
@@ -0,0 +1,19 @@
+import CardProfile from "./CardProfile";
+import Step from './imagens/Step.jpg'
+function Coordination(params) {
+    return(
+        <div id="background" style={{paddingTop: '70px', paddingBottom: '70px'}}>
+            <h2 style={{textAlign:'center'}}><strong>Coordenação</strong></h2>
+            <div style={{display: "flex", justifyContent: 'center', paddingTop:'20px'}}>
+            
+            {/*centralizar a foto ou acima ou na esquerda do card */}
+            <CardProfile
+                name="Stephany Kamarry"
+                profileImageUrl={Step}
+                bio="Coordenadora geral" />
+                
+            </div>
+        </div>
+    );
+}
+export default Coordination;
