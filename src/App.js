f048b189b986e9b236dc1c346b239b40e68ee814
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-08 02:53:08 -0300

    atualizando e definindo rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Route, Routes} from "react-router-dom"
+import FirstSection from "./Components/FirstSection";
+import Header from "./Components/Header";
+import AboutTheProject from './Components/AboutTheProject';
+import CardComponent from './Components/CardComponent'
+import Coordination from './Components/Coordination'
+import Footer from './Components/Footer'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="youtube.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          learn how 
-        </a>
-      </header>
-    </div>
+<Routes>
+  <Route path="/" element={[<Header />,
+   <FirstSection />, <AboutTheProject />,
+    <CardComponent />, <Coordination />,
+    <Footer/>]} />
+
+<Route path="/cursos" element={[<Header />,
+    <AboutTheProject />,
+    <CardComponent />, <Coordination />,
+    <Footer/>]} />
+</Routes>
   );
 }
 
