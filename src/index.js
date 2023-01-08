d8a544013cfb221afe513576034e8e69558a5444
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-08 09:37:29 -0300

    Renderização de componentes transferida para o App.js acrescido das rotas

--- src/index.js
+++ src/index.js
@@ -4,35 +4,17 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 //<App />
-import Header from './Components/Header';
-import FirstSection from './Components/FirstSection';
-import AboutTheProject from './Components/AboutTheProject';
-import CardComponent from './Components/CardComponent'
-import Coordination from './Components/Coordination'
-import Footer from './Components/Footer'
+import { BrowserRouter } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   //Aqui listamos os componentes para renderizar na tela
   <React.StrictMode>
-    <section>
-    <Header />
-    </section>
-    <section>
-      <FirstSection />
-    </section>
-    <section>
-    <AboutTheProject />
-    </section>
-    <section>
-    <CardComponent />
-    </section>
-    <section>
-    <Coordination />
-    </section>
-<section>
-<Footer />
-</section>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
 
 
 
