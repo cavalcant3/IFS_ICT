cd0577620b16028454769b7464b76aafce63be33
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-09 13:40:52 -0300

    usando o componente criado

---
+++ src/Components/CoursePage/SectionNewAnnouncements.js
@@ -0,0 +1,10 @@
+
+function SectionNewAnnouncements(props) {
+    return(
+        <div id="background" style={{ backgroundColor: '#f6f6f6' }}>
+            <h2 style={{ textAlign: 'center' }}><strong>{props.title}</strong></h2>
+
+        </div>
+    );
+}
+export default SectionNewAnnouncements;
