751dce67e6dddfafe8ec1636f0c1e0313ac0d206
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-09 14:50:26 -0300

    adicionando pequena formatação na legenda do card

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -5,11 +5,13 @@ import { useNavigate } from 'react-router-dom';
 function CardComponent() {
     let navigate = useNavigate();
     return (
-         <div href="#cursos" className='Container' style={{ backgroundColor: '#f6f6f6', paddingTop: '70px', paddingBottom: '70px' }}>
+         <div href="#cursos" className='ContainerCardCourse' style={{ backgroundColor: '#f6f6f6', paddingTop: '70px', paddingBottom: '70px' }}>
 
              <div style={{ }}>
                  <h3 style={{ textAlign:'center'}}><strong>Cursos e inscrições</strong></h3>
                  <br />
+                 <p style={{ textAlign:'center', color:'#a2a2a2', lineHeight: '1.6', fontSize:'16px'}}><i>Conheça mais dos cursos oferecidos,
+                  professores ministrantes, bolsistas e o cronograma das aulas e datas de inscrições.</i></p>
              </div>
              <div className='cardCarrossel' style={{ display:'flex', justifyContent:'center' }}>
                  <Card id="sld" style={{ width: '18rem' }}>
@@ -17,7 +19,7 @@ function CardComponent() {
                      <Card.Body>
                         <Card.Title>Inteligência Artificial</Card.Title>
                          <Card.Text>
-                             Inteligência Artificial (IA) é uma nova ciência técnica para a pesquisa...
+                             <i style={{color:'#a2a2a2'}}>Inteligência Artificial (IA) é uma nova ciência técnica para a pesquisa...</i>
                          </Card.Text>
                      </Card.Body>
 
