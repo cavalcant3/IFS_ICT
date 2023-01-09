d4e5e16e6e0af3ae2f0dcc5491144e38bac824e4
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-09 13:40:04 -0300

    criação de componentes

---
+++ src/Components/CoursePage/MatriculasAbertas.js
@@ -0,0 +1,10 @@
+import SectionNewAnnouncements from "./SectionNewAnnouncements";
+
+function MatriculasAbertas(params) {
+ return(
+    <SectionNewAnnouncements
+    title="Matriculas Abertas" />
+ );
+}
+
+export default MatriculasAbertas;
