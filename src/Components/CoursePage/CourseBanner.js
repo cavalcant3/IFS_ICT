a548724799e99cfaa363ec42ee6aacb11001f4a1
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-11 18:23:18 -0300

    alteração no componente CourseBannerdo site

--- src/Components/CoursePage/CourseBanner.js
+++ src/Components/CoursePage/CourseBanner.js
@@ -5,18 +5,11 @@ function CourseBanner() {
     return (
         <section>
             <div className="Banner" style={{}}>
-
                 <div className="Container">
                     {/*Ou certificação?*/}
-                    <h1 className="titulo" style={{ fontSize: '60px' }}>Treinamento: Artificial Intelligence</h1>
-                    <h3 className="titulo" style={{}}><strong>(Certificação HCIA-IA)</strong></h3>
-                    <p className="texto">O Instituto Federal de Sergipe em parceria com a Huawei
-                        oferta gratuitamente a todos os estudantes e profissionais da área de Tecnologia
-                        da Informação e Engenharia do <strong>IFS</strong>, o treinamento para a certificação em Inteligência Artificial.</p>
-
+                    <h1 className="titulo" style={{ fontSize: '35px'}}>Inteligência Artificial </h1>
                 </div>
             </div>
-
         </section>
 
     );
