264e6dc96932dc75b3d97132cbd796a47b4eb009
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-12 17:24:35 -0300

    eliminei os itens da página principal das tabelas e os transformei em componentes

--- src/Components/CoursePage/AboutCourse.js
+++ src/Components/CoursePage/AboutCourse.js
@@ -1,33 +1,31 @@
 
 import { Tab, Tabs } from 'react-bootstrap';
+import DescriptionComponent from './DescriptionComponent'
 
 function AboutCourse() {
     return (
-        <div style={{width:'100%', paddingLeft:'100px', paddingRight:'100px'}}>
+        <div style={{ width: '100%', paddingLeft: '100px', paddingRight: '100px'}}>
             <Tabs
                 defaultActiveKey="profile"
                 id="justify-tab-example"
                 className="mb-3"
                 fill
                 style={{}}
-                
+
             >
-                <Tab eventKey="home" title="Descrição">
-                    <div style={{}}>
-                        <p >No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
-                             frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
-                    </div>
+                <Tab eventKey="home" title="Descrição" >
+                <DescriptionComponent />
                 </Tab>
                 <Tab eventKey="infoAdicional" title="Informação Adicional">
                     <div style={{}}>
                         <p >No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
-                             frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
+                            frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
                     </div>
                 </Tab>
                 <Tab eventKey="resultados" title="Resultados">
                     <div style={{}}>
                         <p >No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
-                             frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
+                            frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
                     </div>
                 </Tab>
 
