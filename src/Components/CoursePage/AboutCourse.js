f1cfbc4817a4b3805428e7a4d7445df862037cce
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-12 14:48:35 -0300

    definindo abas para organizar informações do curso

--- src/Components/CoursePage/AboutCourse.js
+++ src/Components/CoursePage/AboutCourse.js
@@ -1,49 +1,37 @@
 
-
+import { Tab, Tabs } from 'react-bootstrap';
 
 function AboutCourse() {
     return (
-        <div style={{ display: 'flex', alignItems: 'center', paddingLeft: '150px', paddingRight: '100px', paddingTop: '70px', paddingBottom: '70px' }}>
-            {/*<img src={image} alt="Description of image" style={{marginRight: '10px', marginLeft: '10px'}} /> */}
-            <span>
-                <h2 style={{}}><strong>Para quem é esse curso?</strong></h2>
-                <br />
-                <span >
-                    <div >
-                        <p style={{ display: 'block', marginBlockStart: '1em', marginBlockEnd: '1em', marginInlineStart: '0px', marginInlineEnd: '0px', textAlign: 'justify', width: '750px' }}>
-                            Este curso será aberto à comunidade e portanto poderá abranger alunos de nível superior bem como profissionais
-                            de mercado da área de Tecnologia
-                            da Informação (TI) e Engenharias com o objetivo de formar futuros profissionais certificados em HCIA-AI.
-                        </p>
+        <div style={{width:'100%', paddingLeft:'100px', paddingRight:'100px'}}>
+            <Tabs
+                defaultActiveKey="profile"
+                id="justify-tab-example"
+                className="mb-3"
+                fill
+                style={{}}
+                
+            >
+                <Tab eventKey="home" title="Descrição">
+                    <div style={{}}>
+                        <p >No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
+                             frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
                     </div>
-                    <br />
-                    <h2><strong>Capacitação Artificial Intelligence (Certificação HCIA IA 3.0)</strong></h2>
-                    <br />
-                    <p style={{ display: 'block', marginBlockStart: '1em', marginBlockEnd: '1em', marginInlineStart: '0px', marginInlineEnd: '0px', textAlign: 'justify', width: '750px' }}>
-                        Nos últimos anos, com o aumento da capacidade computacional de processamento
-                        e armazenamento e com a disponibilidade de enormes quantidades de dados disponíveis (Big Data
-                        ) tem-se observado a adoção de técnicas de Inteligência Artificial como em ciência de dados (Data Science)
-                        e aprendizagem profunda possibilitando às organizações prever ou até mesmo lidar com situações inicialmente inesperadas.
-                    </p>
-                    <br />
-                    <p style={{ display: 'block', marginBlockStart: '1em', marginBlockEnd: '1em', marginInlineStart: '0px', marginInlineEnd: '0px', textAlign: 'justify', width: '750px' }}>
-                        Dada a escassez de recursos humanos qualificados em técnicas avançadas de inteligência artificial,
-                        a HUAWEI, que atua no segmento de Inteligência Artificial (IA) desenvolvendo tecnologias de ponta
-                        para serviços em nuvem, estruturas de desenvolvimento, dentre outros temas, criou um programa de capacitação
-                        e certificação de forma a promover e replicar os conhecimentos técnicos necessários para
-                        a formação de estudantes e profissionais desse segmento e com o objetivo de avaliar e certificar tais profissionais.
-                    </p>
-                    <br />
-                    <p style={{ display: 'block', marginBlockStart: '1em', marginBlockEnd: '1em', marginInlineStart: '0px', marginInlineEnd: '0px', textAlign: 'justify', width: '750px' }}>
-                        Nesse contexto, uma das opções de certificação providas pela HUAWEI
-                        é a HCIA-AI, que tem como objetivo validar o conhecimento e as habilidades
-                        para desenvolvimento e configuração de ferramentas com uso de técnicas de Inteligência Artificial.
-                        O certificado é de extrema importância para a padronização do conhecimento em matemática essencial
-                        e linguagem básica de programação em IA, aprendizagem profunda
-                        (Deep Learning), reconhecimento de imagem, reconhecimento de fala e diálogo homem-máquina.
-                    </p>
-                </span>
-            </span>
+                </Tab>
+                <Tab eventKey="infoAdicional" title="Informação Adicional">
+                    <div style={{}}>
+                        <p >No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
+                             frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
+                    </div>
+                </Tab>
+                <Tab eventKey="resultados" title="Resultados">
+                    <div style={{}}>
+                        <p >No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
+                             frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
+                    </div>
+                </Tab>
+
+            </Tabs>
 
 
         </div>
