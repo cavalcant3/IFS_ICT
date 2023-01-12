37743c104b02563968b80d68acdaef550fed7dd0
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-12 20:08:55 -0300

    adicionando conteúdo da nova aba

--- src/Components/CoursePage/AditionalInformation.js
+++ src/Components/CoursePage/AditionalInformation.js
@@ -1,9 +1,33 @@
 import './AditionalINformation.css'
 
 function AditionalInformation() {
-    return(
-        <div></div>
+    {/**Isso aqui é pra ser uma tabela */}
+    return (
+
+        <div className='containerrr'>
+            <hr />
+            <p style={{ fontWeight: '700' }}>Modalidade</p>
+            <hr />
+
+            
+            <p style={{ fontWeight: '700' }}>Modalidade	100% Online </p>
+            <hr />
+            <p style={{ fontWeight: '700' }}>Turno	Segunda a Sexta - Noite </p>
+            <hr />
+            <p style={{ fontWeight: '700' }}>Horários	Webconferência (via Google Meet) das 19:00 - 20:30 (presença online obrigatória)</p>
+            <hr />
+            <p style={{ fontWeight: '700' }}>Pré-Requisitos	Inglês básico e Conhecimento prévio em programação(não obrigatório)</p>
+            <hr />
+            <p style={{ fontWeight: '700' }}>Certificação internacional	Validade 3 anos após prova de certificação.</p>
+            <hr />
+            <p style={{ fontWeight: '700' }}> Requisitos para Voucher	Mooc Exam com pontuação acima de 700 pontos
+             e conclusão de curso preparatório fornecido pelo Instituto Federal de Sergipe.</p>
+            <hr />
+            <p style={{ fontWeight: '700' }}>Prova de certificação	Forma remota</p>
+            <hr />
+            
+        </div>
     );
-    
+
 }
 export default AditionalInformation;
