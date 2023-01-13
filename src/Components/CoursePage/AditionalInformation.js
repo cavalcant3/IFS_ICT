2dbd10240a390cc2f54f3dcac35885afd1ed09cd
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-13 18:58:38 -0300

    adicionando classes em linhas da tabela

--- src/Components/CoursePage/AditionalInformation.js
+++ src/Components/CoursePage/AditionalInformation.js
@@ -1,49 +1,49 @@
 import './AditionalINformation.css'
 
 function AditionalInformation() {
-    {/**Isso aqui é pra ser uma tabela */ }
     return (
 
         <div className='containerrr'>
-            <h3>Informação adicional</h3>
+            <p style={{fontSize:'20px'}}><strong>Informação adicional</strong></p>
             <table>
                 <tr>
-                    <td><strong>Modalidade</strong></td>
+                    
+                    <td className='TitleTable'><strong>Modalidade</strong></td>
                     <td>100% Online</td>
 
                 </tr>
                 <tr>
-                    <td><strong>Turno</strong></td>
-                    <td>Segunda a Sexta - Noite</td>
+                    <td className='TitleTable'><strong>Turno</strong></td>
+                <td>Segunda a Sexta - Noite</td>
 
-                </tr>
-                <tr>
-                    <td><strong>Turno</strong></td>
-                    <td>Segunda a Sexta - Noite</td>
-                </tr>
-                <tr>
-                    <td><strong>Horários</strong></td>
-                    <td>Webconferência (via Google Meet) das 19:00 - 20:30 (presença online obrigatória)</td>
-                </tr>
-                <tr>
-                    <td><strong>Pré-Requisitos</strong></td>
-                    <td>Inglês básico e Conhecimento prévio em programação(não obrigatório)</td>
-                </tr>
-                <tr>
-                    <td><strong>Prova de certificação</strong></td>
-                    <td>Forma remota</td>
-                </tr>
-                <tr>
-                    <td><strong>Requisitos para Voucher</strong></td>
-                    <td>Mooc Exam com pontuação acima de 700 pontos e conclusão
-                        de curso preparatório fornecido pelo Instituto Federal de Sergipe.</td>
-                </tr>
-                <tr>
-                    <td><strong>Certificação internacional</strong></td>
-                    <td>Validade 3 anos após prova de certificação.</td>
-                </tr>
-            </table>
-        </div>
+            </tr>
+            <tr>
+                <td className='TitleTable'><strong>Turno</strong></td>
+                <td>Segunda a Sexta - Noite</td>
+            </tr>
+            <tr>
+                <td className='TitleTable'><strong>Horários</strong></td>
+                <td>Webconferência (via Google Meet) das 19:00 - 20:30 (presença online obrigatória)</td>
+            </tr>
+            <tr>
+                <td className='TitleTable'><strong>Pré-Requisitos</strong></td>
+                <td>Inglês básico e Conhecimento prévio em programação(não obrigatório)</td>
+            </tr>
+            <tr>
+                <td className='TitleTable'><strong>Prova de certificação</strong></td>
+                <td>Forma remota</td>
+            </tr>
+            <tr>
+                <td className='TitleTable'><strong>Requisitos para <br />Voucher</strong></td>
+                <td>Mooc Exam com pontuação acima de 700 pontos e conclusão
+                    de curso preparatório fornecido pelo Instituto<br /> Federal de Sergipe.</td>
+            </tr>
+            <tr>
+                <td className='TitleTable'><strong>Certificação <br />internacional</strong></td>
+                <td>Validade 3 anos após prova de certificação.</td>
+            </tr>
+        </table>
+        </div >
     );
 
 }
