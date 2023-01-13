96198785bbb06956648866677ef4cb35776dbac1
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-13 19:22:36 -0300

    passando o componente

--- src/Components/CoursePage/AboutCourse.js
+++ src/Components/CoursePage/AboutCourse.js
@@ -1,20 +1,22 @@
 
 import { Tab, Tabs } from 'react-bootstrap';
 import DescriptionComponent from './DescriptionComponent'
+import AditionalInformation from './AditionalInformation'
+import './AboutCourse.css'
 
 function AboutCourse() {
     {/* Tela de abas para informações dos cursos */}
     return (
-        <div style={{ width: '100%', paddingLeft: '100px', paddingRight: '100px' }}>
+        <div style={{marginLeft: '220px', marginRight: '220px' }}>
             <Tabs
                 defaultActiveKey="profile"
-                id="justify-tab-example"
+                id="uncontrolled-tab-example"
                 className="mb-3"
                 fill
                 style={{}}
 
             >
-                <Tab eventKey="home" title="Descrição" >
+                <Tab eventKey="home" title="Descrição">
                     <div style={{display: 'flex', alignItems: 'center', justifyContent:'center'}}>
                         <p style={{ textAlign: 'center', fontSize: '16px', color: '#a2a2a2', width: '800px', paddingBottom:'20px' }}>No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
                             frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore,
@@ -24,8 +26,8 @@ function AboutCourse() {
                 </Tab>
                 <Tab eventKey="infoAdicional" title="Informação Adicional">
                     <div style={{}}>
-                        <p >No curso são abordados os temas relacionados à Inteligência Artificial, Aprendizado de Máquinas, Aprendizado Profundo,
-                            frameworks para aprendizado profundo, PyTorch, TensorFlow, Mindspore, Arquitetura Ascend, plataforma Open AI e Huawei Cloud.</p>
+                        <AditionalInformation />
+                        
                     </div>
                 </Tab>
                 <Tab eventKey="resultados" title="Resultados">
