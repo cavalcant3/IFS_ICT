71ab2a1f99da81284108858170389c2403acbcaa
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-13 15:37:18 -0300

    adicionando informações da forma correta

--- src/Components/CoursePage/AditionalInformation.js
+++ src/Components/CoursePage/AditionalInformation.js
@@ -1,31 +1,48 @@
 import './AditionalINformation.css'
 
 function AditionalInformation() {
-    {/**Isso aqui é pra ser uma tabela */}
+    {/**Isso aqui é pra ser uma tabela */ }
     return (
 
         <div className='containerrr'>
-            <hr />
-            <p style={{ fontWeight: '700' }}>Modalidade</p>
-            <hr />
+            <h3>Informação adicional</h3>
+            <table>
+                <tr>
+                    <td><strong>Modalidade</strong></td>
+                    <td>100% Online</td>
 
-            
-            <p style={{ fontWeight: '700' }}>Modalidade	100% Online </p>
-            <hr />
-            <p style={{ fontWeight: '700' }}>Turno	Segunda a Sexta - Noite </p>
-            <hr />
-            <p style={{ fontWeight: '700' }}>Horários	Webconferência (via Google Meet) das 19:00 - 20:30 (presença online obrigatória)</p>
-            <hr />
-            <p style={{ fontWeight: '700' }}>Pré-Requisitos	Inglês básico e Conhecimento prévio em programação(não obrigatório)</p>
-            <hr />
-            <p style={{ fontWeight: '700' }}>Certificação internacional	Validade 3 anos após prova de certificação.</p>
-            <hr />
-            <p style={{ fontWeight: '700' }}> Requisitos para Voucher	Mooc Exam com pontuação acima de 700 pontos
-             e conclusão de curso preparatório fornecido pelo Instituto Federal de Sergipe.</p>
-            <hr />
-            <p style={{ fontWeight: '700' }}>Prova de certificação	Forma remota</p>
-            <hr />
-            
+                </tr>
+                <tr>
+                    <td><strong>Turno</strong></td>
+                    <td>Segunda a Sexta - Noite</td>
+
+                </tr>
+                <tr>
+                    <td><strong>Turno</strong></td>
+                    <td>Segunda a Sexta - Noite</td>
+                </tr>
+                <tr>
+                    <td><strong>Horários</strong></td>
+                    <td>Webconferência (via Google Meet) das 19:00 - 20:30 (presença online obrigatória)</td>
+                </tr>
+                <tr>
+                    <td><strong>Pré-Requisitos</strong></td>
+                    <td>Inglês básico e Conhecimento prévio em programação(não obrigatório)</td>
+                </tr>
+                <tr>
+                    <td><strong>Prova de certificação</strong></td>
+                    <td>Forma remota</td>
+                </tr>
+                <tr>
+                    <td><strong>Requisitos para Voucher</strong></td>
+                    <td>Mooc Exam com pontuação acima de 700 pontos e conclusão
+                        de curso preparatório fornecido pelo Instituto Federal de Sergipe.</td>
+                </tr>
+                <tr>
+                    <td><strong>Certificação internacional</strong></td>
+                    <td>Validade 3 anos após prova de certificação.</td>
+                </tr>
+            </table>
         </div>
     );
 
