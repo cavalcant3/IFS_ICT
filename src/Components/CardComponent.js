868f4c9db904b1d1e7b147b28becf5c0a18afb60
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-15 09:25:55 -0300

    comentando o uso do link to

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,6 +1,6 @@
 import Card from 'react-bootstrap/Card';
 import inteligenciaCard from './imagens/inteligenciaCard.jpg'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './CardComponent.css'
 
 function CardComponent() {
@@ -12,25 +12,29 @@ function CardComponent() {
             <div>
                 <h3 style={{ textAlign: 'center' }}><strong>Cursos e inscrições</strong></h3>
 
-                <p  className='IntroductionLegend' style={{ textAlign: 'center', color: '#a2a2a2', lineHeight: '1.6', fontSize: '16px' }}><i>Conheça mais dos cursos oferecidos,
+                <p className='IntroductionLegend' style={{ textAlign: 'center', color: '#a2a2a2', lineHeight: '1.6', fontSize: '16px' }}><i>Conheça mais dos cursos oferecidos,
                     professores ministrantes, bolsistas, o cronograma das aulas e datas de inscrições.</i></p>
                 <div className='cardCarrossel' style={{ display: 'flex', justifyContent: 'center' }}>
                     <Card id="sld" style={{ width: '18rem' }}>
-                        <Card.Img alt="picture of course" variant='top'  src="https://www.totvs.com/wp-content/uploads/2019/06/o-que-e-inteligencia-artificial.jpg"  />
+                        <Card.Img alt="picture of course" variant='top' src="https://www.totvs.com/wp-content/uploads/2019/06/o-que-e-inteligencia-artificial.jpg" />
                         <Card.Body>
                             <Card.Title>Inteligência Artificial</Card.Title>
                             <Card.Text>
-                                <i style={{ color: '#a2a2a2'}}>Inteligência Artificial (IA) é uma nova ciência técnica para a pesquisa...</i>
+                                <i style={{ color: '#a2a2a2' }}>Inteligência Artificial (IA) é uma nova ciência técnica para a pesquisa...</i>
                             </Card.Text>
                         </Card.Body>
 
                         <Card.Body>
+{/**                            <Link
+                            to={'/cursos'}
+                            /> */}
                             <Card.Link
                                 onClick={() => {
                                     navigate("/cursos");
                                 }}
                                 style={{ display: 'flex', justifyContent: 'center' }}
-                                Link to class="btn btn-success">Saiba mais</Card.Link>
+                                Link to class="btn btn-success">Saiba mais
+                            </Card.Link>
                         </Card.Body>
                     </Card>
                 </div>
