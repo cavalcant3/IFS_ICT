83cd693268cfffc7468d1666af7ae9f125a9005f
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-16 20:48:48 -0300

    Organizando

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import AboutCourse from './Components/CoursePage/AboutCourse';
 import ProfessoresEbolsistas from './Components/CoursePage/ProfessoresEbolsistas';
 import MatriculasAbertas from './Components/CoursePage/MatriculasAbertas';
 import AditionalInformation from './Components/CoursePage/AditionalInformation';
+import DescriptionComponent from "./Components/CoursePage/DescriptionComponent";
 {/*<MatriculasAbertas />,*/ }
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
       <ProfessoresEbolsistas />,
       <Footer />]} />
 
-      <Route path="/testando" element={<AditionalInformation />}>
+      <Route path="/testando" element={[<Header />,<CourseBanner />,<AboutCourse />,<ProfessoresEbolsistas />, <Footer />]}>
 
       </Route>
     </Routes>
