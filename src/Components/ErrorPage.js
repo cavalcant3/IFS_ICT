819e76515a733bd4c4250ec4d4904edacaa7fa4c
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-17 11:21:10 -0300

    upload errorPage

---
+++ src/Components/ErrorPage.js
@@ -0,0 +1,14 @@
+import imageError from '../Components/imagens/errorPage.png'
+
+function ErrorPage() {
+    return (
+
+       
+            <div style={{display:'flex'}}>
+                <img style={{ width:'80%', alignItems:'center'}} src={imageError} />
+            </div>
+       
+    );
+}
+
+export default ErrorPage;
