5af8fc1c5dfb8e01f961ba711b96d7604de24dcc
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-17 11:43:06 -0300

    saiba mais

--- src/Components/FirstSection.js
+++ src/Components/FirstSection.js
@@ -2,7 +2,7 @@ import './FirstSection.css'
 import videoBGMp4 from './videos/preview.mp4';
 import videoBGOgg from './videos/preview.ogg';
 import videoBGWebm from './videos/preview.webm';
-
+import angle from './imagens/angle.png';
 
 function FirstSection() {
   {/**Video AQUI */ }
@@ -25,7 +25,10 @@ function FirstSection() {
           <p>O Instituto Federal de Educação Ciência e Tecnologia de Sergipe (IFS), em parceria com Huawei,
              está ofertando um curso online
              com o objetivo de preparar alunos e profissionais na área de Inteligência Artificial.</p>
+             <div style={{marginTop:'5%', fontSize:'13px', fontWeight: '1.6'}}>saiba mais</div>
+             <i> <img  style={{width: '17px'}} src={angle}/></i>
         </div>
+      
         <div className='buttonHomePage'>
           
         </div>
