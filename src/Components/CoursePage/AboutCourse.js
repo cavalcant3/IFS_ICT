802aa601ba6c0ae3d105987b6982f520cee467e1
Matheus Cavalcante <jmatheusoc269@gmail.com>
2023-01-31 18:43:16 -0300

    Update AboutCourse.js

--- src/Components/CoursePage/AboutCourse.js
+++ src/Components/CoursePage/AboutCourse.js
@@ -94,7 +94,7 @@ function AboutCourse() {
 
                     </div>
                 </Tab>
-                <Tab eventKey="resultados" title="Acesse aqui o edital">
+                <Tab eventKey="resultados" title="Confira o edital">
                     <div style={{}}>
                         {/**FALTA ORGANIZAR OS RESULTADOS */}
                         
